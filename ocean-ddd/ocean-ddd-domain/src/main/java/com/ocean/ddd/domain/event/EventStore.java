96265eaa722c2283699bdcc2cc37f6213c5a73b0
yang.zhang <yang.zhang@vhall.com>
2022-07-13 16:44:59 +0800

    完善领域模块

---
+++ ocean-ddd/ocean-ddd-domain/src/main/java/com/ocean/ddd/domain/event/EventStore.java
@@ -0,0 +1,28 @@
+package com.ocean.ddd.domain.event;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @Description: 事件存储
+ * @Author: yang.zhang
+ * @Date: 2022/7/13 16:20
+ */
+public interface EventStore {
+    /**
+     * 存储事件
+     *
+     * @param aggregateId
+     * @param newEvents
+     * @param baseVersion
+     */
+    void store(UUID aggregateId, List<Event> newEvents, int baseVersion);
+
+    /**
+     * 加载事件
+     *
+     * @param aggregateId
+     * @return
+     */
+    List<Event> load(UUID aggregateId);
+}
--- ocean-ddd/ocean-ddd-domain/src/main/java/com/ocean/ddd/domain/valueobject/ValueObject.java
+++ ocean-ddd/ocean-ddd-domain/src/main/java/com/ocean/ddd/domain/valueobject/ValueObject.java
@@ -1,9 +1,28 @@
 package com.ocean.ddd.domain.valueobject;
 
+import cn.hutool.core.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  * @Description: 值对象
  * @Author: yang.zhang
  * @Date: 2022/7/12 17:02
  */
 public abstract class ValueObject {
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
 }
