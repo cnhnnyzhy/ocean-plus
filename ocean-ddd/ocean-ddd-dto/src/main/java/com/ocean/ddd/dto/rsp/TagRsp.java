7ec978d29a9574120d5a8d8d76fe5c3cb7562478
cnhnnyzhy <cnhnnyzhy@163.com>
2025-02-23 22:58:44 +0800

    添加git忽略文件

--- ocean-ddd/ocean-ddd-dto/src/main/java/com/ocean/ddd/dto/rsp/TagRsp.java
+++ ocean-ddd/ocean-ddd-dto/src/main/java/com/ocean/ddd/dto/rsp/TagRsp.java
@@ -2,12 +2,14 @@ package com.ocean.ddd.dto.rsp;
 
 import com.ocean.ddd.dto.Rsp;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @Description: TagRsp
  * @Author: yang.zhang
  * @Date: 2022/7/12 14:48
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class TagRsp extends Rsp {
     private Long id;
